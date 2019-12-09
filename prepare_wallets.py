17719c3fb09e091e270b8a8f96927326156ea3a2
Dmytro Tkachuk <dmitry.tkk@gmail.com>
2019-12-09 19:15:03 +0200

    SKALE-1830 update repo to support new skale-node and skale-admin

--- prepare_wallets.py
+++ prepare_wallets.py
@@ -19,38 +19,43 @@
 
 import os
 import logging
-import json
 
 from skale import Skale
-from skale.utils.helper import private_key_to_address, init_default_logger
-from skale.utils.account_tools import init_wallet, generate_accounts
+from skale.wallets import Web3Wallet
+from skale.utils.web3_utils import init_web3
+from skale.utils.helper import init_default_logger
+from skale.utils.account_tools import generate_accounts
 
 logger = logging.getLogger(__name__)
-
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-ABI_FILEPATH = os.path.join(CURRENT_DIR, 'data.json')
-
-n_wallets = os.environ['N_WALLETS']
-skale_amount = os.environ['SKALE_AMOUNT']
-eth_amount = os.environ['ETH_AMOUNT']
-
 init_default_logger()
 
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 ENDPOINT = os.environ['ENDPOINT']
+ABI_FILEPATH = os.path.join(CURRENT_DIR, 'manager.json')
+ETH_PRIVATE_KEY = os.environ['ETH_PRIVATE_KEY']
 
-skale = Skale(ENDPOINT, ABI_FILEPATH)
-base_wallet = init_wallet()
+N_WALLETS = os.environ['N_WALLETS']
+SKALE_AMOUNT = os.environ['SKALE_AMOUNT']
+ETH_AMOUNT = os.environ['ETH_AMOUNT']
 
-accounts = generate_accounts(skale, base_wallet, n_wallets, skale_amount, eth_amount, True)
+web3 = init_web3(ENDPOINT)
+wallet = Web3Wallet(ETH_PRIVATE_KEY, web3)
+skale = Skale(ENDPOINT, ABI_FILEPATH, wallet)
 
-for i, account  in enumerate(accounts):
+
+def save_wallet_info(i, account):
     filename = f'wallet-{i}.txt'
     filepath = os.path.join(CURRENT_DIR, 'wallets', filename)
-    #content = f'#!/usr/bin/env bash\nexport ETH_PRIVATE_KEY={account["private_key"]}'
-    content = account["private_key"]
     with open(filepath, 'w') as outfile:
-         logger.info(f'Saving info to {filename}')
-         outfile.write(content)
+        logger.info(f'Saving info to {filename}')
+        outfile.write(account["private_key"])
 
 
-#save_info('node_wallets.json', result)
+def prepare_wallets():
+    accounts = generate_accounts(skale, skale.wallet, N_WALLETS, SKALE_AMOUNT, ETH_AMOUNT, True)
+    for i, account in enumerate(accounts):
+        save_wallet_info(i, account)
+
+
+if __name__ == "__main__":
+    prepare_wallets()
