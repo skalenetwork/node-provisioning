02d5ce69abfefecf4e56ff90ea6cf0258fc56863
badrogger <ivan@skalelabs.com>
2019-12-10 13:34:46 +0200

    Add sleep before register operation to make sure that transaction manager is started

--- scripts/provision_node.sh
+++ scripts/provision_node.sh
@@ -5,6 +5,9 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo apt install -y python3-pip
 
 pip3 install -r $CURRENT_DIR/requirements.txt
+
+sleep 60s
+
 python3 $CURRENT_DIR/transfer_funds.py
 
 skale node register --name $NODE_NAME --ip $NODE_IP --port $NODE_PORT
