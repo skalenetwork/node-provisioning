1399d9c1d05bda1a15781189b35d434d03f88af5
badrogger <ivan@skalelabs.com>
2020-11-19 20:10:30 +0200

    SKALE-3529 Refactor trunsfer_funds script

--- ansible/files/transfer_funds.py
+++ ansible/files/transfer_funds.py
@@ -19,45 +19,24 @@
 
 import os
 import logging
-import ast
-import subprocess
-from subprocess import PIPE
 
 from skale import Skale
 from skale.wallets import Web3Wallet
 from skale.utils.web3_utils import init_web3
 from skale.utils.helper import init_default_logger
-from skale.utils.account_tools import (send_ether, send_tokens,
-                                       check_ether_balance,
-                                       check_skale_balance)
+from skale.transactions.tools import send_eth_with_skale
+from skale.utils.web3_utils import to_checksum_address
 
 
 BASE_DIR = os.getenv('BASE_DIR')
 ENDPOINT = os.getenv('ENDPOINT')
 ABI_FILEPATH = os.path.join(BASE_DIR, 'manager.json')
-ETH_PRIVATE_KEY = os.environ['ETH_PRIVATE_KEY']
-
-GAS_COMMISSION_FACTOR = 0.9
-
+ETH_PRIVATE_KEY = os.getenv('ETH_PRIVATE_KEY')
+ADDRESS = os.getenv('ADDRESS')
+AMOUNT = float(os.getenv('AMOUNT'))
 
 logger = logging.getLogger(__name__)
 init_default_logger()
-
-
-def run_cmd(cmd, env={}, shell=False):
-    logger.info(f'Running: {cmd}')
-    res = subprocess.run(cmd, shell=shell, stdout=PIPE,
-                         stderr=PIPE, env={**env, **os.environ})
-    if res.returncode:
-        logger.error('Error during shell execution:')
-        logger.error(res.stderr.decode('UTF-8').rstrip())
-        raise subprocess.CalledProcessError(res.returncode, cmd)
-    return res
-
-
-def format_output(res):
-    return (res.stdout.decode('UTF-8').rstrip(),
-            res.stderr.decode('UTF-8').rstrip())
 
 
 def init_web3_skale():
@@ -66,39 +45,11 @@ def init_web3_skale():
     return Skale(ENDPOINT, ABI_FILEPATH, wallet)
 
 
-def get_node_wallet_address():
-    cmd = 'skale wallet info -f json'
-    res = run_cmd(cmd, shell=True)
-    out, _ = format_output(res)
-    wallet_info = ast.literal_eval(out)
-    return wallet_info['address']
-
-
-def send_funds(skale, address, skale_amount, eth_amount):
-    send_tokens(skale, skale.wallet, address, skale_amount)
-    send_ether(skale.web3, skale.wallet, address, eth_amount)
-    check_ether_balance(skale.web3, address)
-    check_skale_balance(skale, address)
-
-
-def get_transfer_amount(skale):
-    address = skale.wallet.address
-    ether_balance = check_ether_balance(skale.web3, address)
-    skale_balance = check_skale_balance(skale, address)
-    return float(ether_balance) * GAS_COMMISSION_FACTOR, skale_balance
-
-
-def _run_tm_manager(skale):
-    send_ether(skale.web3, skale.wallet, skale.wallet.address, 0)
-
-
 def main():
     skale = init_web3_skale()
-    _run_tm_manager(skale)  # todo: temporary measure, remove later
-    address = get_node_wallet_address()
-    # eth_amount, skale_amount = get_transfer_amount(skale)
-    eth_amount, skale_amount = 0.2, 0
-    send_funds(skale, address, skale_amount, eth_amount)
+    address = to_checksum_address(ADDRESS)
+    wei_amount = skale.web3.toWei(AMOUNT, 'ether')
+    send_eth_with_skale(skale, address, wei_amount)
 
 
 if __name__ == "__main__":
