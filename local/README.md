80ff22388cbb6e4d7e82bb0eaa3ddba57f7caf37
Dmytro <dmitry.tkk@gmail.com>
2020-12-14 16:44:29 +0200

    SKALE-3628 Update Vagrant provision, add hosts file generator

---
+++ local/README.md
@@ -0,0 +1,9 @@
+# Bare metal provisioning
+
+## Vagrant provision
+
+Run this playbook to spin up Vagrant VMs, provision them and generate hosts file for node-provisioning:
+
+```bash
+ansible-playbook -i inventory run-vms.yaml
+```
---
+++ local/ansible/files/generate_hosts.py
@@ -0,0 +1,17 @@
+import os
+
+SUBNET = os.getenv('SUBNET')
+NUM_OF_VMS = int(os.getenv('NUM_OF_VMS'))
+BASE_IP = int(os.getenv('BASE_IP'))
+SSH_USER = os.getenv('SSH_USER')
+
+NODES_PREFIX = '[nodes]\n'
+
+def generate_hosts():
+    with open('hosts', 'w') as f:
+        f.write(NODES_PREFIX)
+        for i in range(1, NUM_OF_VMS):
+            f.write(f'node{i} ansible_host={SUBNET}.{BASE_IP+i} ansible_ssh_user={SSH_USER}\n')
+
+if __name__ == "__main__":
+    generate_hosts()
