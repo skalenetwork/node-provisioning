2ca2d5ff6cb6d6d655e086aa6bc76f0e5c5ef8cf
elvis <alexeymaratovich@gmail.com>
2021-03-19 17:59:01 +0200

    SKLE-3994-add restart vm script after reboot host

---
+++ bare-metal/ansible/files/generate_script.py
@@ -0,0 +1,13 @@
+import os
+
+NUM_OF_VMS = int(os.getenv('NUM_OF_VMS'))
+BASE_NAME = os.getenv('BASE_NAME')
+
+def generate_script():
+    with open('files/restart_vm.sh', 'w') as f:
+        f.write('#! /bin/bash\n')
+        for i in range(1, NUM_OF_VMS+1):
+            f.write(f'VBoxManage startvm {BASE_NAME}-{i} --type headless\n')
+
+if __name__ == "__main__":
+    generate_script()
