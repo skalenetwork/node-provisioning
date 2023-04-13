b6b93b3c84fe6fda058707048193bdfe35969995
badrogger <ivan@skalelabs.com>
2023-04-13 18:08:34 +0000

    Fix ssh connection for bare-metal

--- bare-metal/ansible/files/generate_scripts.py
+++ bare-metal/ansible/files/generate_scripts.py
@@ -1,16 +1,16 @@
 import os
 
 NUM_OF_VMS = int(os.getenv('NUM_OF_VMS'))
-BASE_NAME = os.getenv('BASE_NAME')
+BASE_NAME = os.getenv('VM_BASE_NAME')
 BASE_PATH = os.getenv('BASE_PATH')
 
 
 def generate_script():
-    with open(f'files/restart_vm.sh', 'w') as f:
+    with open('files/restart_vm.sh', 'w') as f:
         f.write('#!/bin/bash\n')
-        for i in range(1, NUM_OF_VMS+1):
+        for i in range(0, NUM_OF_VMS):
             f.write(f'VBoxManage startvm {BASE_NAME}-{i} --type headless\n')
-        
+
 
 if __name__ == "__main__":
     generate_script()
