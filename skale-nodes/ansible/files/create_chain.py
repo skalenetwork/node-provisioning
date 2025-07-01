2a23182d6e128391b673acebc2c4011dcedb9e71
Alex Sheverdin <alex@skalelabs.com>
2025-07-01 15:02:37 +0100

    Create a chain depending on its type

--- skale-nodes/ansible/files/create_chain.py
+++ skale-nodes/ansible/files/create_chain.py
@@ -20,16 +20,11 @@
 import os
 import logging
 from eth_typing import HexStr
-
+from enum import Enum
 from skale import SkaleManager
 from skale.utils.web3_utils import init_web3
 from skale.wallets import Web3Wallet
-from skale.utils.contracts_provision.main import (
-    add_test_permissions,
-    add_test2_schain_type,
-    add_test4_schain_type,
-    create_schain
-)
+from skale.utils.contracts_provision.main import add_test_permissions, create_schain
 
 logger = logging.getLogger(__name__)
 
@@ -37,7 +32,61 @@ CHAIN_NAME = os.environ['CHAIN_NAME']
 ENDPOINT = os.environ['ENDPOINT']
 ETH_PRIVATE_KEY = os.environ['ETH_PRIVATE_KEY']
 MANAGER_CONTRACTS = os.environ['MANAGER_CONTRACTS']
+CHAIN_TYPE = os.environ.get('CHAIN_TYPE', 'SMALL2').upper()
 
+class SchainType(Enum):
+    SMALL2 = [1, 2]
+    SMALL4 = [1, 4]
+    SMALL = [1, 16]
+    MEDIUM2 = [16, 2]
+    MEDIUM4 = [16, 4]
+    MEDIUM = [16, 16]
+    LARGE2 = [128, 2]
+    LARGE4 = [128, 4]
+    LARGE = [128, 16]
+    TEST0_4 = [0, 4]
+    TEST4_2 = [32, 2]
+    TEST4_4 = [32, 4]
+    TEST4 = [32, 16]
+    EMPTY = [0, 0]
+
+def get_chain_type_params(chain_type: str) -> list:
+    try:
+        return SchainType[chain_type].value
+    except KeyError:
+        print(f"Error: '{chain_type}' is not a valid SchainType. "
+              f"Available types are: {[member.name for member in SchainType]}")
+        raise
+
+def add_schain_type(part_of_node, number_of_nodes):
+    return skale.schains_internal.add_schain_type(
+        part_of_node, number_of_nodes
+    )
+
+def get_schain_type_id(skale, type_parameters):
+    n = skale.schains_internal.contract.functions.numberOfSchainTypes().call()
+    for idx in range(1, n + 1):
+        params = skale.schains_internal.get_schain_type(idx)
+        print(f'PARAMS: {params}')
+        if type_parameters == params:
+            return idx
+    return None
+
+def prepare_and_create_chain(skale, chain_type=CHAIN_TYPE) -> None:
+    add_test_permissions(skale)
+    type_params = get_chain_type_params(chain_type)
+    type_id = get_schain_type_id(skale, type_params)
+    if type_id is None:
+        print(f'sChain type {chain_type} '
+              f'with {type_params[0]}, {type_params[1]} not found. Adding to contracts...')
+        add_schain_type(type_params[0], type_params[1])
+        type_id = get_schain_type_id(skale, type_params)
+
+    create_schain(
+        skale,
+        schain_name=CHAIN_NAME,
+        schain_type=type_id,
+    )
 
 def init_skale_manager(
     endpoint: str, alias_or_address: str, eth_private_key: HexStr
@@ -47,18 +96,7 @@ def init_skale_manager(
     return SkaleManager(endpoint, alias_or_address, wallet)
 
 
-def bootstrap_mirage(endpoint: str, alias_or_address: str, eth_private_key: HexStr) -> None:
-    skale = init_skale_manager(endpoint, alias_or_address, eth_private_key)
-    add_test_permissions(skale)
-    add_test2_schain_type(skale)
-    add_test4_schain_type(skale)
-
-    create_schain(
-        skale,
-        schain_name=CHAIN_NAME,
-        schain_type=1,
-    )
-
-
 if __name__ == '__main__':
-    bootstrap_mirage(ENDPOINT, MANAGER_CONTRACTS, HexStr(ETH_PRIVATE_KEY))
+    skale = init_skale_manager(ENDPOINT, MANAGER_CONTRACTS, HexStr(ETH_PRIVATE_KEY))
+    prepare_and_create_chain(skale)
+
