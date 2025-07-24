78d36f004ee67027baac88b37dbca6fe187206c5
Alex Sheverdin <alex@skalelabs.com>
2025-07-24 15:41:17 +0100

    Move `rng_address` variable to constants

--- skale-nodes/ansible/files/set_rng.py
+++ skale-nodes/ansible/files/set_rng.py
@@ -8,18 +8,16 @@ from skale import FairManager
 ENDPOINT = os.getenv('ENDPOINT')
 FAIR_CONTRACTS = os.getenv('FAIR_CONTRACTS')
 ETH_PRIVATE_KEY = os.getenv('ETH_PRIVATE_KEY')
+RNG_ADDRESS = '0x0000000000000000000000000000000000000018'
 
 
 def init_fair() -> FairManager:
     web3 = init_web3(ENDPOINT)
     wallet = Web3Wallet(HexStr(ETH_PRIVATE_KEY), web3)
-    if not FAIR_CONTRACTS:
-        raise ValueError('FAIR_CONTRACTS is not set')
     return FairManager(ENDPOINT, FAIR_CONTRACTS, wallet)
 
 
 if __name__ == '__main__':
     fair = init_fair()
-    rng_address = '0x0000000000000000000000000000000000000018'
     skale_rng = fair.committee.skale_rng()
-    fair.committee.set_rng(rng_address)
+    fair.committee.set_rng(RNG_ADDRESS)
